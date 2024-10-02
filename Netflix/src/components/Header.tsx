31f35b05e1cedf72aa236606c308094b9dc6604d
Kamil Romanowicz <kamil.romanowicz95@gmail.com>
2024-10-02 13:19:39 +0200

    added more styling to header

--- Netflix/src/components/Header.tsx
+++ Netflix/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import DropdownIcon from "../assets/drop_down_list_icon_155460.png";
+import NotflixLogo from "../assets/notflix.webp"
 
 export default function Header() {
   const [search, setSearch] = useState("");
@@ -8,12 +9,12 @@ export default function Header() {
   // const [isSearching, setIsSearching] = useState(false);
 
   const navigate = useNavigate();
-  const goToCategories = (event: React.MouseEvent<HTMLLIElement>) => {
+  const goToCategories = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     navigate("/categories");
   };
 
-  const goToBookmarks = (event: React.MouseEvent<HTMLLIElement>) => {
+  const goToBookmarks = (event: React.MouseEvent<HTMLElement>) => {
     event.preventDefault();
     navigate("/favs");
   };
@@ -33,56 +34,59 @@ export default function Header() {
   };
 
   return (
-    <section className="grid grid-cols-3 grid-rows-2 m-1 p-1 w-full h-[15rem] place-items-center bg-red-700 border-8 rounded-2xl border-black">
-      <h1 className="col-span-1 text-black place-items-center">
-        Netflix from wish
-      </h1>
-      <div className="relative col-start-3 bg-gray-600 rounded p-1">
+    <section className="grid grid-cols[1fr, 1fr, 2fr, 1fr] grid-rows-1 m-1 p-1 w-full h-[12rem] place-items-center bg-black ">
+      <div className="col-start-3 col-span-1 w-[20rem]">
+        <img src={NotflixLogo} alt="" />
+      </div>
+      <div className="relative col-start-1 row-start-1 place-self-start-center m-2 bg-gray-600 rounded p-1">
         <img
           src={DropdownIcon}
           onClick={toggleDropdown}
           className="w-10 h-10 cursor-pointer"
           alt=""
         />
-
         {/* Dropdown Menu */}
         <div
-          className={`absolute left-0 mt-2 w-48 bg-gray-600 border rounded-lg shadow-lg transition-all duration-300 ease-in-out origin-top transform ${
+          className={`absolute left-0 mt-2 w-48 bg-white border rounded-lg shadow-lg transition-all duration-300 ease-in-out origin-top transform ${
             isOpen ? "opacity-100 scale-y-100" : "opacity-0 scale-y-0"
           }`}
           style={{ transformOrigin: "top" }}
         >
           <ul className="flex flex-col">
             <li
-              className="cursor-pointer hover:text-blue-300 hover:bg-gray-400 hover:shadow-md p-2 rounded"
+              className=" text-black cursor-pointer hover:text-blue-300 hover:bg-gray-700 hover:shadow-md p-2 rounded"
               onClick={goToCategories}
             >
               Categories
             </li>
             <li
-              className="cursor-pointer hover:text-blue-300 hover:bg-gray-400 hover:shadow-md p-2 rounded"
+              className="text-black cursor-pointer hover:text-blue-300 hover:bg-gray-700 hover:shadow-md p-2 rounded"
               onClick={goToBookmarks}
             >
               Bookmarks
             </li>
-            <li className="cursor-pointer hover:text-blue-300 hover:bg-gray-400 hover:shadow-md p-2 rounded">
+            <li className="text-black cursor-pointer hover:text-blue-300 hover:bg-gray-700 hover:shadow-md p-2 rounded">
               Profile
             </li>
-            <li className="cursor-pointer hover:text-blue-300 hover:bg-gray-400 hover:shadow-md p-2 rounded">
+            <li className="text-black cursor-pointer hover:text-blue-300 hover:bg-gray-700 hover:shadow-md p-2 rounded">
               Settings
             </li>
           </ul>
         </div>
       </div>
-
-      <section className="col-start-2 row-start-2">
+      <section className="col-start-2 row-start-1">
         <input
           type="text"
-          className="w-full h-1/3 border rounded p-2"
+          className="w-[15rem] h-1/3 bg-white text-black border rounded-3xl p-2"
           placeholder="Search"
           value={search}
           onChange={handleSubmit}
         />
+      </section>
+      <section className="flex col-start-4">
+        <h2 className="mr-6 text-gray-400 cursor-pointer hover:text-blue-300">Home</h2>
+        <h2 onClick={goToCategories} className="mr-6 text-gray-400 cursor-pointer hover:text-blue-300">Categories</h2>
+        <h2 onClick={goToBookmarks} className="mr-6 text-gray-400 cursor-pointer hover:text-blue-300">Bookmarks</h2>
       </section>
     </section>
   );
