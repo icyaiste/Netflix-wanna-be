3f1574b88262fdb29d140f915c5f8d49f237b23c
Menarg <erik.hahn.nilsson@gmail.com>
2024-10-02 13:12:35 +0200

    added bookmarks skeleton

---
+++ Netflix/src/components/Bookmark.tsx
@@ -0,0 +1,14 @@
+
+function mark() {
+    
+}
+
+function Bookmark() {
+    return (
+        <div>
+            <button onClick={mark}></button>
+        </div>
+    )
+}
+
+export default Bookmark
