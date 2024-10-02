f22791be6d9b8d636f1ceecd0371490e2624dc40
Menarg <erik.hahn.nilsson@gmail.com>
2024-10-02 15:27:39 +0200

    foldered

--- Netflix/src/pages/bookmarked/Bookmarked.tsx
+++ Netflix/src/pages/bookmarked/Bookmarked.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Header from "../../components/Header";
+import Header from "../../components/header/Header";
 import Carousel from "../../components/carousel/Carousel";
 
 
