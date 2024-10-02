fccdfad45a901fa10df7946eed91139ac9b3b4f2
Benji-ha <143506737+Benji-ha@users.noreply.github.com>
2024-10-02 15:09:04 +0200

    Created categories logic (#17)
    * Created categories logic
    
    * Prettier updated

---
+++ Netflix/src/pages/ChosenGenre/ChosenGenre.tsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom";
+import moviesData from "../../movies/movies.json";
+
+function ChosenGenre() {
+  const { genreName } = useParams();
+
+  const filteredMovies = moviesData.filter(
+    (movie) => movie.genre === genreName,
+  );
+
+  return (
+    <div>
+      <h1>{genreName} Movies</h1>
+      <div className="grid grid-cols-3 gap-x-3 gap-y-3">
+        {filteredMovies.map((movie) => (
+          <div
+            key={movie.title}
+            className="box-border h-64 w-64 bg-cover bg-center rounded-lg shadow-xl text-black "
+          >
+            <img
+              src={movie.thumbnail}
+              alt={movie.title}
+              className="h-full w-full object-cover rounded-lg"
+            />
+            <h2>{movie.title}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ChosenGenre;
--- Netflix/src/router/router.tsx
+++ Netflix/src/router/router.tsx
@@ -3,6 +3,7 @@ import App from "../App";
 import Bookmarked from "../pages/bookmarked/Bookmarked";
 import Categories from "../pages/categories/Categories";
 import FilmInfo from "../pages/filmInfo/FilmInfo";
+import ChosenGenre from "../pages/ChosenGenre/ChosenGenre";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +21,10 @@ const router = createBrowserRouter([
   {
     path: "/info",
     element: <FilmInfo />,
+  },
+  {
+    path: "/genre/:genreName",
+    element: <ChosenGenre />,
   },
 ]);
 
