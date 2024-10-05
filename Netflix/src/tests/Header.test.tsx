56af7951e66cde738b9cdb87d105c75192a88fc7
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2024-10-05 14:40:14 +0200

    cretes tests folder and basic files

--- Netflix/src/tests/Header.test.tsx
+++ Netflix/src/tests/Header.test.tsx
@@ -1,1 +1,7 @@
-// import { test, expect } from "vitest";
+import { test, expect } from "vitest";
+// import { render } from "@testing-library/react";
+// import Header from "../components/header/Header";
+
+test("it should render correctly", () => {
+  expect(true).toBe(true);
+});
