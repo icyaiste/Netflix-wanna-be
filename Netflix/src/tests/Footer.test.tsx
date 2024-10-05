ff939f579d249c8cb825da739f1713d78b5989ed
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2024-10-05 14:40:14 +0200

    cretes tests folder and basic files

--- Netflix/src/tests/Footer.test.tsx
+++ Netflix/src/tests/Footer.test.tsx
@@ -1,1 +1,7 @@
-// import { test, expect } from "vitest";
+import { test, expect } from "vitest";
+// import { render } from "@testing-library/react";
+// import Footer from "../components/footer/Footer";
+
+test("renders correctly", () => {
+  expect(true).toBe(true);
+});
