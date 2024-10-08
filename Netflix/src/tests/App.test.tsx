1df7b8f3988b6a575f3fba5cbd1d25119ae524a2
Aiste <icy.aiste@gmail.com>
2024-10-08 15:54:55 +0200

    tog bort kommentarerna pga Prettier klagar

--- Netflix/src/tests/App.test.tsx
+++ Netflix/src/tests/App.test.tsx
@@ -3,7 +3,3 @@ import { test, expect } from 'vitest';
 test('True to be true', () => {
   expect(true).toBe(true);
 });
-
-// Rendera ut app
-// Hämta första filmen i trending
-// Kolla att den inte förekommer i recommended.
