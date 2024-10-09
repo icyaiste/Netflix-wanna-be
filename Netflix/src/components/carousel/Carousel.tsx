d61b54d4fcb9b0348af1c3347e3a9ad90e5cb1e7
Kamil Romanowicz <143507032+kemorsky@users.noreply.github.com>
2024-10-09 10:09:43 +0200

    fixed positioning issues between search results and carousel

--- Netflix/src/components/carousel/Carousel.tsx
+++ Netflix/src/components/carousel/Carousel.tsx
@@ -25,53 +25,52 @@ function Carousel({ data, labelledBy }: CarouselProps) {
   };
 
   return (
-    <div className='relative'>
+    <div className="relative">
       <section
-          className="z-[-1] flex justify-center items-center w-full overflow-hidden"
-          aria-label={labelledBy}
+        className="z-[-1] flex justify-center items-center w-full overflow-hidden"
+        aria-label={labelledBy}
+      >
+        {/* Left Arrow */}
+        <BsArrowLeftCircleFill
+          className="absolute left-4 w-8 h-8 text-white rounded-full shadow-lg hover:cursor-pointer z-10"
+          onClick={prevSlide}
+        />
+        {/* Carousel Slides */}
+        <div
+          className="flex transition-transform duration-500"
+          style={{ transform: `translateX(-${currentSlide * 5}%)` }}
         >
-          {/* Left Arrow */}
-          <BsArrowLeftCircleFill
-            className="absolute left-4 w-8 h-8 text-white rounded-full shadow-lg hover:cursor-pointer z-10"
-            onClick={prevSlide}
-          />
-          {/* Carousel Slides */}
-          <div
-            className="flex transition-transform duration-500"
-            style={{ transform: `translateX(-${currentSlide * 5}%)` }}
-          >
-            {data.map((movie, index) => (
-              <div
-                key={index}
-                className="flex-col flex-shrink-0 w-[225px] h-[330px] p-[7px]"
-              >
-                <Bookmark movie={movie} />
-                <img
-                  src={movie.thumbnail}
-                  alt={movie.title}
-                  className="w-full h-[90%] object-contain rounded-lg shadow-md"
-                  onClick={() => handleNavigateToFilmInfo(movie.title)}
-                />
-                <section className="h-[35px] w-[95%] flex justify-between items-center">
-                  <p className="ml-2 text-red-600 text-xl font-medium justify-self-start">
-                    {movie.rating}
-                  </p>
-                  <h2 className=" text-xl font-semibold w-[50px] mr-[35%]">
-                    {movie.year}
-                  </h2>
-                </section>
-              </div>
-            ))}
-          </div>
+          {data.map((movie, index) => (
+            <div
+              key={index}
+              className="flex-col flex-shrink-0 w-[225px] h-[330px] p-[7px]"
+            >
+              <Bookmark movie={movie} />
+              <img
+                src={movie.thumbnail}
+                alt={movie.title}
+                className="w-full h-[90%] object-contain rounded-lg shadow-md"
+                onClick={() => handleNavigateToFilmInfo(movie.title)}
+              />
+              <section className="h-[35px] w-[95%] flex justify-between items-center">
+                <p className="ml-2 text-red-600 text-xl font-medium justify-self-start">
+                  {movie.rating}
+                </p>
+                <h2 className=" text-xl font-semibold w-[50px] mr-[35%]">
+                  {movie.year}
+                </h2>
+              </section>
+            </div>
+          ))}
+        </div>
 
-          {/* Right Arrow */}
-          <BsArrowRightCircleFill
-            className="absolute right-4 w-8 h-8 text-white rounded-full shadow-lg hover:cursor-pointer z-10"
-            onClick={nextSlide}
-          />
-        </section>
+        {/* Right Arrow */}
+        <BsArrowRightCircleFill
+          className="absolute right-4 w-8 h-8 text-white rounded-full shadow-lg hover:cursor-pointer z-10"
+          onClick={nextSlide}
+        />
+      </section>
     </div>
-  
   );
 }
 
