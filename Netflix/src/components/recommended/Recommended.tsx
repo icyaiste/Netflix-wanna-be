6d2f09f322253449771b4762449830b75e0f1c42
Aiste <icy.aiste@gmail.com>
2024-10-10 13:35:27 +0200

    new logic in recommended

--- Netflix/src/components/recommended/Recommended.tsx
+++ Netflix/src/components/recommended/Recommended.tsx
@@ -3,10 +3,10 @@ import { Movie } from '../../interfaces/Interfaces.ts';
 import data from '../../movies/movies.json';
 
 function Recommended() {
-  // Function to select even-indexed and shuffled movies
+  // Function to select uneven-indexed and shuffled movies
   const selectMovies = (allMovies: Movie[], limit: number): Movie[] => {
-    const unevenIndexMovies = allMovies.filter((_, index) => index % 2 !== 0); // Filter uneven-indexed movies
-    const shuffledMovies = unevenIndexMovies.sort(() => Math.random() - 0.5); // Shuffle the filtered array
+    const nonTrendingMovies = allMovies.filter((movie) => !movie.isTrending); // Filter uneven-indexed movies
+    const shuffledMovies = nonTrendingMovies.sort(() => Math.random() - 0.5); // Shuffle the filtered array
     return shuffledMovies.slice(0, limit); // Slice the shuffled movies to the limit
   };
 
@@ -15,7 +15,7 @@ function Recommended() {
     return <h1>Loading...</h1>;
   }
 
-  // Select 5 even-indexed movies for trending
+  // Select 5 uneven-indexed movies for trending
   const recMovies = selectMovies(data, 10);
 
   return (
