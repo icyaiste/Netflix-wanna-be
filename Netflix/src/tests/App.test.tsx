79dd775024ab0fd9d1362cfe48f46c52ca0c857c
Menarg <erik.hahn.nilsson@gmail.com>
2024-10-11 18:12:07 +0200

    Bookmarks tested

--- Netflix/src/tests/App.test.tsx
+++ Netflix/src/tests/App.test.tsx
@@ -1,17 +1,15 @@
 import { test, expect, beforeEach, describe } from 'vitest';
 import App from '../App';
-import { render, screen } from '@testing-library/react';
-import Carousel from '../components/carousel/Carousel';
-import movies from '../movies/movies.json';
+import { render, screen, within } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import BookmarkProvider from '../context/BookmarkContext';
 import userEvent from '@testing-library/user-event';
+
 
 beforeEach(() => {
   render(
     <BookmarkProvider>
       <MemoryRouter>
-        <Carousel data={movies} labelledBy="Trending" />
         <App />
       </MemoryRouter>
     </BookmarkProvider>,
@@ -19,36 +17,26 @@ beforeEach(() => {
 });
 
 test('trending movies are not in recommended movies', () => {
-  <MemoryRouter>
-    render(
-    <App />
-    );
-  </MemoryRouter>;
 
   // Find all the movies in the "Trending" section by aria-label
   const trendingMovies = screen.getAllByLabelText('Trending');
   expect(trendingMovies.length).toBeGreaterThan(0); // Ensure there are movies in "Trending"
 
   // Get the first trending movie
-  const firstTrendingMovie = screen.getAllByAltText('The Shawshank Redemption');
+  const firstTrendingMovie = screen.getAllByAltText('Fight Club');
   expect(firstTrendingMovie[0]).toBeInTheDocument();
 
   // Now check that the first trending movie is NOT in the "Recommended" section
   const recommendedMovies = screen.getAllByLabelText('Recommended');
 
   recommendedMovies.forEach((movie) => {
-    expect(movie).not.toHaveTextContent('The Shawshank Redemption');
+    expect(movie).not.toHaveTextContent('Fight Club');
   });
 });
 
 
 describe('bookmarked functionality',() =>{
-test('when Bookmarked movie is clicked, it changes image (to yellow)', async () => {
-  <MemoryRouter>
-    render(
-    <App />
-    );
-  </MemoryRouter>;
+test('when Bookmarked movie is clicked, it changes icon (to yellow)', async () => {
 
 const user = userEvent.setup();
 
@@ -58,71 +46,64 @@ const user = userEvent.setup();
   await user.click(bookmarkedBtn);
 
   //When Bookmarked, button changes color to yellow
-  expect(bookmarkedBtn).toHaveClass('text-yellow-500');
+  expect(bookmarkedBtn).toHaveClass('text-xl bg-transparent text-yellow-500 transition-all');
 });
 
-test.only('bookmarked movies appear in Bookmarked carousel', async () => {
-  <MemoryRouter>
-    render(
-    <App />
-    );
-  </MemoryRouter>;
+test('bookmarked movies appear in Bookmarked carousel', async () => {
 
 const user = userEvent.setup();
 
-  //Find how many movies 'The Shawshank Redemption' there are on page
-  let findShanks = screen.getAllByAltText('The Shawshank Redemption');
-  const numberOfShanks = findShanks.length;
-  expect(findShanks).toHaveLength(numberOfShanks);
+  //Find how many movies 'Fight Club' there are on page
+  const findMovies = screen.getAllByAltText('The Godfather: Part II');
+  expect(findMovies).toHaveLength(1);
 
-  //find Bookmarked button on 'The Shawshank Redemption' movie and click it
-  const bookmarkedBtn = screen.getAllByRole('button')[0];
+  //find Bookmarked button on 'Fight Club' movie and click it
+  const bookmarkedBtn = screen.getAllByLabelText('Add Bookmark')[0];
   expect(bookmarkedBtn).toBeInTheDocument();
   await user.click(bookmarkedBtn);
 
   //When Bookmarked, button changes color to yellow
-  expect(bookmarkedBtn).toHaveClass('text-yellow-500');
+  expect(bookmarkedBtn).toHaveClass('text-xl bg-transparent text-yellow-500 transition-all');
 
-  //Check if 'The Shawshank Redemption' appeared in Bookmarked carousel
-  findShanks = screen.getAllByAltText('The Shawshank Redemption');
-  expect(findShanks).toHaveLength(numberOfShanks + 1);
+   //Check if 'Fight Club' appeared in Bookmarked carousel
+  const findMoviesAgain = screen.getAllByAltText('The Godfather: Part II');
+  expect(findMoviesAgain).toHaveLength(2);
 });
 
-test.only('movie disappears from Bookmarked carousel when buttons Bookmarked is clicked again',async () => {
-    
-  <MemoryRouter>
-    render(
-    <App />
-    );
-  </MemoryRouter>;
-
+test('movie disappears from Bookmarked carousel when buttons Bookmarked is clicked again', async () => {
 
 const user = userEvent.setup();
-  //Find how many movies 'The Shawshank Redemption' there are on page
-  const findShanks = screen.getAllByAltText('The Shawshank Redemption');
-  screen.debug();
-  expect(findShanks).toHaveLength(1);
-  
-  
-  //find Bookmarked button on 'The Shawshank Redemption' movie and click it
-  const bookmarkedBtn = screen.getAllByRole('button')[0];
+
+  //Find how many movies 'Fight Club' there are on page
+  let findMovies = screen.getAllByAltText('The Godfather: Part II');
+  expect(findMovies).toHaveLength(1);
+ 
+  //find Bookmarked button on 'Fight Club' movie and click it
+  const bookmarkedBtn = screen.getAllByLabelText('Add Bookmark')[0];
   expect(bookmarkedBtn).toBeInTheDocument();
   await user.click(bookmarkedBtn);
-
+ 
   //When Bookmarked, button changes color to yellow
-  expect(bookmarkedBtn).toHaveClass('text-yellow-500');
+  expect(bookmarkedBtn).toHaveClass('text-xl bg-transparent text-yellow-500 transition-all');
+ 
+  //Check if 'Fight Club' appeared in Bookmarked carousel
+  findMovies = screen.getAllByAltText('The Godfather: Part II');
+  expect(findMovies).toHaveLength(2);
 
-  // //Check if 'The Shawshank Redemption' appeared in Bookmarked carousel
-  // findShanks = screen.getAllByAltText('The Shawshank Redemption');
-  // expect(findShanks).toHaveLength(numberOfShanks + 1);
+  // Find the 'The Godfather: Part II' movie card/container
+  const GodfatherMovie = screen.getAllByAltText('The Godfather: Part II')[1].closest('div');
 
-  //  //find Bookmarked button on 'The Shawshank Redemption' movie in Bookmarked carousel and click it
-  //  const bookmarkCarouselBtn = screen.getAllByRole('button')[numberOfShanks + 1];
-  //  expect(bookmarkCarouselBtn).toBeInTheDocument();
-  //  await user.click(bookmarkCarouselBtn);
+  // Ensure godfatherMovie is not null
+  expect(GodfatherMovie).not.toBeNull();
+  if (!GodfatherMovie) return;
 
-  //  //Check if clicked movie disappeared from Bookmarked carousel
-  //  findShanks = screen.getAllByAltText('The Shawshank Redemption');
-  //  expect(findShanks).toHaveLength(numberOfShanks +5);
+  // Find the bookmark button within that specific movie card
+  const bookmarkBtnWithinMovie = within(GodfatherMovie).getByRole('button');
+
+  await user.click(bookmarkBtnWithinMovie);
+
+  //Check if clicked movie disappeared from Bookmarked carousel
+   findMovies = screen.getAllByAltText('The Godfather: Part II');
+   expect(findMovies).toHaveLength(1);
 })
 });
