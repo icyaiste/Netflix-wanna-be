9d41d04e71948f6ba8a1283309887541dd00dea8
Aiste <icy.aiste@gmail.com>
2024-10-13 22:00:10 +0200

    set base option

--- Netflix/vite.config.ts
+++ Netflix/vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Netflix-wanna-be/',
   test: {
     globals: true,
     environment: "jsdom",
